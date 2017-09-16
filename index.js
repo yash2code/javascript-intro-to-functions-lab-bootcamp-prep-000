4ce2d598f10b8fef5b894215d97bd45598808304
yash chaudhary <yash240196@gmail.com>
2017-09-16 04:13:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string.toLowerCase() === string){
+  if(string.toLowerCase() === string)
     return 'I can\'t hear you!'
 }
