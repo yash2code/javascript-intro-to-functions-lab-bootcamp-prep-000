18420053df1edcc93fb807ff10e6323302a52883
yash chaudhary <yash240196@gmail.com>
2017-09-16 04:07:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function logShout(string) {
   console.log(string.toUpperCase())
   return string.toUpperCase()
 }
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+  return string.toLowerCase()
+}
