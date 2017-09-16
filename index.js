1a346a5c32497f77a7a88cdc1e1616bc60e0dd62
yash chaudhary <yash240196@gmail.com>
2017-09-16 04:14:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,4 +19,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string.toLowerCase() === string)
     return 'I can\'t hear you!'
+
+  if(string.toUpperCase() === string)
+    return 'YES INDEED!'
 }
