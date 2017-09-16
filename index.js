7fec984a53b83359988d262fdf687376429d69ce
yash chaudhary <yash240196@gmail.com>
2017-09-16 04:14:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string) {
   if(string.toLowerCase() === string)
     return 'I can\'t hear you!'
 
-  if(string.toUpperCase() === string)
+  else if(string.toUpperCase() === string)
     return 'YES INDEED!'
 }
